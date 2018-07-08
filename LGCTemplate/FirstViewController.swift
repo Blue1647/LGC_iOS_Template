415515795280284b68c4990f7376287f9987fae0
Rakesh Das <rakeshdaskowshik@gmail.com>
2018-07-08 10:17:31 -0500

    Initial Commit

---
+++ LGCTemplate/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LGCTemplate
+//
+//  Created by Rakesh Das on 7/8/18.
+//  Copyright © 2018 Rakesh Das. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
